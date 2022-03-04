45bb173948759d8080523e55c26ff4c5bc336575
Deivid <deivid.cardos@gmail.com>
2022-03-03 23:13:08 -0300

    implement: ValueObjects, Validators folder and Price VO

---
+++ PlayPedidos.Domain/ValueObjects/Price.cs
@@ -0,0 +1,30 @@
+﻿namespace PlayPedidos.Domain.ValueObjects
+{
+	public enum Currency
+	{
+		BRL = 1,
+		USD = 2,
+		EUR = 3,
+	}
+
+	public class Price
+	{
+		public decimal Value { get; set; }
+		public Currency Currency { get; set; }
+
+		public Price()
+		{
+		}
+
+		public Price(decimal value)
+		{
+			Value = value;
+		}
+
+		public Price(decimal value, Currency currency)
+		{
+			Value = value;
+			Currency = currency;
+		}
+	}
+}
