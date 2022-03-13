b70447bc8b25a8467ded0305066d2b34b9a81098
Deivid <deivid.cardos@gmail.com>
2022-03-13 19:12:11 -0300

    implement: create two datetimes to control Created and Updated status.

--- PlayPedidos.Domain/Entities/Entity.cs
+++ PlayPedidos.Domain/Entities/Entity.cs
@@ -6,12 +6,19 @@ namespace PlayPedidos.Domain.Entities
 	public abstract class Entity
 	{
 		public int ID { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
 
 		[NotMapped]
 		public ValidationResult ValidationResult { get; set; }
 
 		public Entity()
 		{
+			var now = DateTime.Now;
+			CreatedAt = now;
+			UpdatedAt = now;
+
+			ValidationResult = new ValidationResult();
 		}
 
 		public virtual bool IsValid()
